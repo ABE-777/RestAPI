7ba0c08af04dc2d4611b40990946aa0a1828d53f
ABE-777 <abe-777@mail.ru>
2018-10-01 17:22:24 +0300

    Update SqlQueries.sql

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -1,13 +1,13 @@
 ﻿
---1. Запрос для получения выборки вида: Дата (сутки), Колличество новых Paste за сутки
---Результат: Количество Paste созданных в определённую дату
+--1. Query to get data in following format: Date, Count of new Pastes for the date
+--Result: Count of Pastes created on specific date
 SELECT CreateDate as [Date], COUNT(*) as [Count]
 FROM dbo.Paste
 GROUP BY CreateDate
 
 
---2. Запрос для получения выборки вида: Дата (сутки), Самый последний Paste к которому был доступ в сутках
---Результат: Paste к которым обращались позже всех по дням их созднания
+--2. Query to ge data in following format: Date, The latest Paste which was requested at specific date
+--Result: Paste which was requested last by the dates of creation
 ;WITH pastes AS
 (
   SELECT CreateDate as [Date], Content, Identifier,  RowNum=ROW_NUMBER() OVER (PARTITION BY CreateDate ORDER BY AccessDate DESC)
@@ -16,9 +16,3 @@ GROUP BY CreateDate
 SELECT [Date],Content,Identifier
 FROM pastes
 WHERE RowNum=1
-
---3. Запрос для получения ближайшей даты (суток) относительно указанного параметра X, в которую не было ни одного доступа к любому Paste
-DECLARE @X as DATE
-SET @X = '2018-08-08'
-
-SELECT * FROM  dbo.Paste WHERE AccessDate
